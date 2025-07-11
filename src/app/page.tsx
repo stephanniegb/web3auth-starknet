581fc53ed2c97fe666d1335e3acce6afc5d3088b
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2025-07-11 14:00:32 +0100

    Revert "test avnu"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,15 @@ import {
   getStarkKey,
   calculateAccountAddress,
   getValidPrivateKey,
-  checkGaslessCompatibility,
 } from "./starknetRPC";
 import { fetchBalance, transferToken } from "./tokenOperations";
 import { useEffect, useState } from "react";
-import { Account, PaymasterRpc, RpcProvider } from "starknet";
+import { Account, RpcProvider } from "starknet";
 
 import LoggedIn from "./LoggedIn";
 import LoggedOut from "./LoggedOut";
 
 const isProduction = process.env.NODE_ENV === "production";
-console.log({ isProduction });
 
 export default function Home() {
   // State management
@@ -48,7 +46,6 @@ export default function Home() {
   } = useWeb3AuthDisconnect();
 
   const { provider: web3authProvider } = useWeb3Auth();
-
   const { userInfo } = useWeb3AuthUser();
 
   // StarkNet provider setup
@@ -56,11 +53,6 @@ export default function Home() {
     nodeUrl: isProduction
       ? process.env.NEXT_PUBLIC_STARKNET_JSON_RPC_URL_MAINNET
       : process.env.NEXT_PUBLIC_STARKNET_JSON_RPC_URL_SEPOLIA,
-  });
-
-  const myPaymasterRpc = new PaymasterRpc({
-    nodeUrl: "https://sepolia.paymaster.avnu.fi",
-    headers: { "api-key": process.env.PAYMASTER_API_KEY },
   });
 
   // Effect to get address when provider is available
@@ -95,7 +87,6 @@ export default function Home() {
   useEffect(() => {
     if (address) {
       fetchBalance(address, starknetProvider, setStrkBalance);
-      checkGaslessCompatibility(address);
     }
   }, [address]);
 
@@ -133,19 +124,8 @@ export default function Home() {
         provider: web3authProvider,
       });
 
-      const account = new Account(
-        starknetProvider,
-        address,
-        privateKey,
-        undefined,
-        undefined,
-        myPaymasterRpc
-      );
+      const account = new Account(starknetProvider, address, privateKey);
       setAccount(account);
-
-      const supported = await account.paymaster.getSupportedTokens();
-
-      console.log(supported);
     } catch (error) {
       console.error("Error connecting account:", error);
     } finally {
